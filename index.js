5a99aa18914c625c97ed19c5b7c7b57dd8b847e7
lepamoore <[leonmoore030@gmail.com]>
2023-04-25 17:54:57 +0200

    add hover state for dice icon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getData() {
-    $("#diceContainer").off("click");
+    $("#diceContainer").css('transition', 'none').off("click").off('mouseover');
+    handleDiceMouseout();
     $('#diceContainer').css('cursor', 'progress');
     $('#quote').css('display', 'none');
     $('#loadingContainer').css('display', 'block');
@@ -17,12 +18,25 @@ function getData() {
             document.getElementById('heading').innerHTML = `ADVICE #${result['slip'].id}`;
             document.getElementById('quote').innerHTML = `"${result['slip'].advice}"`;
             $('#diceContainer').css('background-color', '#53FFAA');
-            $('#diceContainer').css('cursor', 'pointer');
+            $('#diceContainer').css('transition', 'all .5s').css('cursor', 'pointer').on("mouseover", handleDiceMouseover);
             $("#diceContainer").on("click", getData);
         }
         setTimeout(reset, 1500);
 
 })};
 
-$("#diceContainer").on("click", getData);
+function handleDiceMouseover() {
+    $("#diceContainer").css('-webkit-box-shadow', '0px 0px 50px 0px rgba(83,255,170,1)');
+    $("#diceContainer").css('-moz-box-shadow', '0px 0px 50px 0px rgba(83,255,170,1)');
+    $("#diceContainer").css('box-shadow', '0px 0px 50px 0px rgba(83,255,170,1)');
+}
+
+function handleDiceMouseout() {
+    $("#diceContainer").css('-webkit-box-shadow', 'none');
+    $("#diceContainer").css('-moz-box-shadow', 'none');
+    $("#diceContainer").css('box-shadow', 'none');
+}
+
+$("#diceContainer").on("click", getData).on("mouseover", handleDiceMouseover).on('mouseout', handleDiceMouseout);
+
 
